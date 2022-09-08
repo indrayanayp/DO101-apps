b8ede1158d922dd87f12fc582cd969701da727b9
indrayanayoga <111721811+indrayanayoga@users.noreply.github.com>
2022-09-08 15:04:08 +0700

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -33,12 +33,12 @@ function shutDown() {
     console.log('Received kill signal, shutting down gracefully');
     server.close(() => {
         console.log('Closed out remaining connections');
-        process.exit(0);
+        //process.exit(0);
     });
 
     setTimeout(() => {
         console.error('Could not close connections in time, forcefully shutting down');
-        process.exit(1);
+        //process.exit(1);
     }, 10000);
 
     connections.forEach(curr => curr.end());

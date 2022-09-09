ec24c236124cba5ba4ecca43cf62472932dbf4c2
indrayanayoga <111721811+indrayanayoga@users.noreply.github.com>
2022-09-09 14:51:48 +0700

    Try server stopper

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,11 +1,51 @@
 var express = require('express');
-app = express();
+const app = express();
+const serverStoppers = require('./serverStoppers')
 
 app.get('/', function (req, res) {
-  res.send('Hello Update!\n');
+  res.send('We are all happy! \n');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/test', function (req, res) {
+  res.send(process.env.TEST123);
+  res.set("Connection", "close");
 });
 
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars!\n');
+});
+
+const server = app.listen(8080, () => console.log('Running…'));
+
+const startGracefulShutdownFunc = serverStoppers.getServerStopFunc(app);
+
+setInterval(() => server.getConnections(
+    (err, connections) => console.log(`${connections} connections currently open`)
+), 1000);
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+let connections = [];
+
+server.on('connection', connection => {
+    connections.push(connection);
+    connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+});
+
+function shutDown() {
+    console.log('Received kill signal, shutting down gracefully');
+    startGracefulShutdownFunc();
+    server.close(() => {
+        console.log('Closed out remaining connections');
+        //process.exit(0);
+    });
+
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        //process.exit(1);
+    }, 10000);
+
+    connections.forEach(curr => curr.end());
+    setTimeout(() => connections.forEach(curr => curr.destroy()), 10000);
+}
